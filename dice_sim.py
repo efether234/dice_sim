ab8f5a61f0adc14b9fff77075e6109f93bcc85c0
Evan Fetherolf <efether234@gmail.com>
2020-03-01 11:56:30 -0800

    Add multi-die functionality.

--- dice_sim.py
+++ dice_sim.py
@@ -3,11 +3,28 @@ A dice simulator.
 """
 from die import Die
 
+def setup():
+    """
+    Create user specified number of dice
+
+    Ask the user how many dice they would like to roll, then return
+    list with that number of dice.
+    """
+    dice = []
+    number_of_dice = input('How many dice would you like to roll? ')
+    try:
+        number_of_dice = int(number_of_dice)
+    except ValueError:
+        print('Oops! Make sure to enter a number!')
+
+    for _ in range(number_of_dice):
+        die = create_die()
+        dice.append(die)
+
+    return dice
+
 def create_die():
-    """
-    Takes an int (sides) from the user, and returns a die with that
-    number of sides.
-    """
+    """Takes an int (sides) from the user, and returns a die with that number of sides."""
     die = None
     while not die:
         sides = input('Please enter a number of sides for the die: ')
@@ -17,21 +34,29 @@ def create_die():
             print('Oops! Make sure to enter a number!')
     return die
 
+def roll_dice(dice):
+    """
+    Roll the dice.
+
+    Takes a list of Die() objects (dice), appends the results of each die's roll()
+    method to a list, then returns the list.
+    """
+    return [die.roll() for die in dice]
+
 def start_sim():
-    """
-    Starts the die rolling sim
-    """
+    """Starts the die rolling sim"""
     print('Welcome to the Dice Simulator')
-    die = create_die()
+    dice = setup()
+
     while True:
         response = input("Type 'r' to roll; type 'e' to exit. ")
         if response == 'e':
             print('Goodbye!')
             break
         elif response == 'r':
-            print(die.roll())
+            print(roll_dice(dice))
         elif response == 's':
-            die = create_die()
+            dice = setup()
         else:
             print('Invalid response.')
 
--- die.py
+++ die.py
@@ -1,20 +1,29 @@
-"""
-A die object.
-"""
 import random
 import math
 class Die():
+    """
+    A die object.
+
+    This die object has one attribute (sides) which represents the number
+    of sides of the die. It has one public method (roll()) which returns
+    an int between 1 and the number of sides inclusive.
+    """
     def __init__(self, sides):
         assert isinstance(sides, int), 'sides must be an int'
         self.sides = sides
+
+    def __str__(self):
+        return 'A ' + str(self.sides) + ' sided die'
+
+    def __repr__(self):
+        return 'Die(' + str(self.sides) + ')'
 
     def roll(self):
         """
         Rolls the die.
 
-        Args:
-            sides: int - number of sides of the die
-        
-        Return: int
+        Rolls the die by generating random float between zero and one,
+        multiplying that by the number of sides, rounding up, and
+        returning the resulting int.
         """
         return math.ceil(random.random() * self.sides)
