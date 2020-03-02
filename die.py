95a62ffd44b522ca65c8ae4b8e0aecd7c3c4b2f0
Evan Fetherolf <efether234@gmail.com>
2020-03-01 18:20:17 -0800

    Remove redundant module, tweak die module.

--- die.py
+++ die.py
@@ -1,12 +1,19 @@
-import random
-import math
+"""
+The die module.
+
+This module contains the Die object, which has one attribute and one method:
+        sides (int) represents the number of sides the die has
+        roll() returns a random int between 1 and the number of sides (inclusive)
+"""
+from random import random
+from math import ceil
 class Die:
     """
     A die object.
 
     This die object has one attribute (sides) which represents the number
     of sides of the die. It has one public method (roll()) which returns
-    an int between 1 and the number of sides inclusive.
+    an int between 1 and the number of sides (inclusive).
     """
     def __init__(self, sides):
         assert isinstance(sides, int), 'sides must be an int'
@@ -26,4 +33,4 @@ class Die:
         multiplying that by the number of sides, rounding up, and
         returning the resulting int.
         """
-        return math.ceil(random.random() * self.sides)
+        return ceil(random() * self.sides)
