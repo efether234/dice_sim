94bb0e03f8411f9248f1f3abb8dde07aeee7512b
Evan Fetherolf <efether234@gmail.com>
2020-03-01 18:01:37 -0800

    Move sim.run_sim() logic into main()

--- dice_sim.py
+++ dice_sim.py
@@ -7,7 +7,21 @@ Author: Evan Fetherolf
 import sim
 
 def main():
-    sim.run_sim()
+    """Starts the die rolling sim"""
+    print('Welcome to the Dice Simulator')
+    dice = sim.create_dice()
+
+    while True:
+        response = input("Type 'r' to roll; type 'e' to exit. ")
+        if response == 'e':
+            print('Goodbye!')
+            break
+        elif response == 'r':
+            print(sim.roll_dice(dice))
+        elif response == 's':
+            dice = sim.create_dice()
+        else:
+            print('Invalid response.')
 
 if __name__ == '__main__':
     main()
--- sim.py
+++ sim.py
@@ -2,13 +2,11 @@
 The dice simulator module.
 
 This module contains all of the functions necessary to run the simulation:
-    __create_dice() creates the dice used for the simulation. It asks the user
+        create_dice() creates the dice used for the simulation. It asks the user
     how many dice to roll, and then how many sides each die should have. It
     returns a list of dice.
-    __roll_dice() actually rolls the dice. It iterates through the list of dice
+        roll_dice() actually rolls the dice. It iterates through the list of dice
     and rolls each one, appending the result to a new list, which it returns.
-    run_sim() contains the loop that actually runs the simulation. It takes user
-    input to create the dice, then to roll the dice.
 """
 from die import Die
 
@@ -47,20 +45,3 @@ def roll_dice(dice):
     method to a list, then returns the list.
     """
     return [die.roll() for die in dice]
-
-def run_sim():
-    """Starts the die rolling sim"""
-    print('Welcome to the Dice Simulator')
-    dice = create_dice()
-
-    while True:
-        response = input("Type 'r' to roll; type 'e' to exit. ")
-        if response == 'e':
-            print('Goodbye!')
-            break
-        elif response == 'r':
-            print(roll_dice(dice))
-        elif response == 's':
-            dice = create_dice()
-        else:
-            print('Invalid response.')
